ecd0a7a78dff13305656796b733b28841b109496
Pablo <pablomata999@gmail.com>
2026-02-10 20:00:43 +0100

    Proyecto_Inicio

---
+++ App_ORM/app/src/test/java/com/example/app_orm_jorge_diaz_diego_diaz_pablo_mata_rodrigo_herrero_examen_t2/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.example.app_orm_jorge_diaz_diego_diaz_pablo_mata_rodrigo_herrero_examen_t2
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
---
+++ App_ORM/settings.gradle.kts
@@ -0,0 +1,23 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "App_ORM_Jorge_Diaz_Diego_Diaz_Pablo_Mata_Rodrigo_Herrero_Examen_T2"
+include(":app")
