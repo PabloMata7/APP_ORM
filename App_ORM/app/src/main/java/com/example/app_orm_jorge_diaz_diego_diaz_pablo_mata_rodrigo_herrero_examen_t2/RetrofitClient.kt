f9e48dc1dae90c79ed435fe5596e15e813b4de4f
DiegoDiazHL <diegodiazhellinl@outlook.com>
2026-02-19 10:16:56 +0100

    c

--- App_ORM/app/src/main/java/com/example/app_orm_jorge_diaz_diego_diaz_pablo_mata_rodrigo_herrero_examen_t2/RetrofitClient.kt
+++ App_ORM/app/src/main/java/com/example/app_orm_jorge_diaz_diego_diaz_pablo_mata_rodrigo_herrero_examen_t2/RetrofitClient.kt
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitClient {
     // IMPORTANTE: Si usas un emulador y el servidor está en tu PC, usa 10.0.2.2
     // Si el servidor es real (MongoDB Atlas), pon aquí la URL de la API
-    private const val BASE_URL = "http://192.168.129.11:3000/"
+    private const val BASE_URL = "http://192.168.32.65:3000/"
 
     val instance: TheVaultApiService by lazy {
         val retrofit = Retrofit.Builder()
